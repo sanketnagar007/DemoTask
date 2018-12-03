c7d560300d8fb6368c1a07bbb81fba3077623764
Anil-Solanki <anilsolanki@Anils-MacBook-Pro.local>
2018-12-04 02:39:08 +0530

    commit message goes here

--- Demo Task/Pods/Target Support Files/Pods-Demo Task/Pods-Demo Task-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Demo_Task : NSObject
-@end
-@implementation PodsDummy_Pods_Demo_Task
-@end
--- Demo Task/Pods/Target Support Files/Pods-Demo TaskTests/Pods-Demo TaskTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Demo_TaskTests : NSObject
-@end
-@implementation PodsDummy_Pods_Demo_TaskTests
-@end
